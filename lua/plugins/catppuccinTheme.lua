389f5437f583fafaa4835183f7f9104c7b627237
Sin Luong <sin@finervision.com>
2024-12-16 16:36:13 +0000

    added new themes.

--- lua/plugins/catppuccinTheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "catppuccin/nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe", -- catppuccin, catppuccin-frappe, catppuccin-macchiato, catppuccin-latte
-    },
-  },
-}
