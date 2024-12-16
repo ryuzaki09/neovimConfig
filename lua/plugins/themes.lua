930d7de495b8a6cee5d61ff1722c487d9368d3bf
Sin Luong <sin@finervision.com>
2024-12-16 16:36:31 +0000

    renamed themes file.

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,33 @@
+return {
+  { "catppuccin/nvim" },
+  {
+    "navarasu/onedark.nvim",
+    opts = { toggle_style_list = { "dark", "cool", "deep", "warm" }, toggle_style_key = "<leader>ts" },
+  },
+  { "Mofiqul/dracula.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "neanias/everforest-nvim" },
+  { "ribru17/bamboo.nvim" },
+  { "tiagovla/tokyodark.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/sonokai" },
+  { "bluz71/vim-nightfly-colors" },
+  { "maxmx03/fluoromachine.nvim" },
+  { "olivercederborg/poimandres.nvim" },
+  { "oxfist/night-owl.nvim" },
+  { "mhartington/oceanic-next" },
+  { "shaunsingh/moonlight.nvim" },
+  { "samharju/synthweave.nvim" },
+  { "nyngwang/nvimgelion" },
+  { "Tsuzat/NeoSolarized.nvim" },
+  { "embark-theme/vim" },
+  { "Yazeed1s/oh-lucy.nvim" },
+  { "alexmozaidze/palenight.nvim" },
+  { "sontungexpt/witch" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe", -- catppuccin, catppuccin-frappe, catppuccin-macchiato, catppuccin-latte
+    },
+  },
+}
